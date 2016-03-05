ce385b1b2ea6d5421d865dd0d31c071cdbcb3a2a
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-05 00:51:45 -0500

    push from laptop 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,14 @@ function makeButtons(){
 	var animals = ["lion","tiger","bear","zebra","llama","cow","horse","ostrich","pig","squirrel","hamster"];
 	
 	$('#animalFinder').empty();
-	
+
 	for (var i = 0; i < animals.length; i++){
 		var x = $('<button class="col-sm-6 col-md-4 col-lg-2 btn btn-default btn-sm">');
 
 		x.addClass('animal');
 		x.attr('data-name', animals[i]);
 		x.text(animals[i]);
-		$('#animalGifs').append(x);
+		$('#animalPicker').append(x);
 	}
 
 	console.log("buttons made!");	
