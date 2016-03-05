7a1eaa5464651c3bbe71f41174b1b4863f24c0ef
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-05 00:43:10 -0500

    push from laptop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
+
+
+function makeButtons(){
+	var animals = ["lion","tiger","bear","zebra","llama","cow","horse","ostrich","pig","squirrel","hamster"];
+	
+	$('#animalFinder').empty();
+	
+	for (var i = 0; i < animals.length; i++){
+		var x = $('<button class="col-sm-6 col-md-4 col-lg-2 btn btn-default btn-sm">');
+
+		x.addClass('animal');
+		x.attr('data-name', animals[i]);
+		x.text(animals[i]);
+		$('#animalGifs').append(x);
+	}
+
+	console.log("buttons made!");	
+}
+
+makeButtons();
+
 // http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 
-
-function render(){
-	var gif = "";
-	var queryURL="http://api.giphy.com/v1/gifs/search?q=" +gifapi_key=dc6zaTOxFJmzC";
-
-	
+function searchGifs(){
+	//sea
 }
