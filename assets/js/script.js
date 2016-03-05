893d1bb4f755336d7e5ca31262be703bdab62747
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-05 12:24:17 -0500

    work in class, still won't append/prepend!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,33 @@
 
+var animals = ['lion','tiger','bear'];
+var x = $('<button>');
+var animalButton = $('#animalButtons')
 
 function makeButtons(){
-	var animals = ["lion","tiger","bear","zebra","llama","cow","horse","ostrich","pig","squirrel","hamster"];
 	
-	$('#animalFinder').empty();
+	//why is animals undefined? 
 
 	for (var i = 0; i < animals.length; i++){
-		var x = $('<button class="col-sm-6 col-md-4 col-lg-2 btn btn-default btn-sm">');
 
 		x.addClass('animal');
 		x.attr('data-name', animals[i]);
 		x.text(animals[i]);
-		$('#animalPicker').append(x);
+
+		animalButton.append(x);
+
+		console.log(animals[i] + " button made!");	
+
 	}
 
-	console.log("buttons made!");	
 }
 
-makeButtons();
+
+
 
 // http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 
 function searchGifs(){
-	//sea
+	$('#animalFinder').empty();
 }
+
+makeButtons();
