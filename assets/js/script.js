fcea8935efc7f3e3faa6ff553334d7e949f9910f
@angryjenkins <matt@angryjenkins.com>
2016-03-05 00:10:37 -0500

    push from desktop

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+
+
+function render(){
+	var gif = "";
+	var queryURL="http://api.giphy.com/v1/gifs/search?q=" +gifapi_key=dc6zaTOxFJmzC";
+
+	
+}
