7de6efe84c0004b1df02eacd6f4fd43182d629c3
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-07 14:20:46 -0500

    push from work 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,17 @@ function makeButtons(){
 for(var i=0;i<animals.length;i++){makeButtons()};
 
 
-function addButtons(){
-	//TBC
-}
+$('#animalButtons').on('click', function(){
+	
+	var newAnimal = $('#addAnimal').val().trim();
+	var animalButton = $('#animalButtons');
 
+	animals.push(newAnimal);
+
+	makeButtons();
+	// Creates	 a new button on click
+	return false;
+});
 
 
 
